6a2321d42983a648e0bbddd9100c9df0e570673d
Viktor Hercinger <hercinger.viktor@gmail.com>
2019-03-08 21:52:07 +0100

    added proxy functions

---
+++ class_proxy.py
@@ -0,0 +1,170 @@
+class _deleted(object):
+    pass
+
+
+class _proxied_value(object):
+    def __init__(self, base, name, instances):
+        self._base = base
+        self._name = name
+        self._instances = instances
+
+    def __get__(self, instance, owner):
+        state = self._instances.get_state(instance)
+        if self._name in state:
+            result = state[self._name]
+
+        elif self._name in self._base.__dict__:
+            result = self._base.__dict__[self._name]
+            owner = self._base
+            if instance is not None:
+                instance = self._instances.get_instance(instance)
+
+        else:
+            assert 0, "unreachable code"
+
+        if result is _deleted:
+            raise AttributeError(
+                "type object {!r} has no attribute {!r}".format(
+                    owner.__name__, self._name
+                )
+            )
+
+        if hasattr(result, "__get__"):
+            result = result.__get__(instance, owner)
+
+        return result
+
+    def __set__(self, instance, value):
+        state = self._instances.get_state(instance)
+        state[self._name] = value
+
+    def __delete__(self, instance):
+        state = self._instances.get_state(instance)
+        state[self._name] = _deleted
+
+
+class _instance_wrapper(object):
+    def __init__(self):
+        self._wrapped_objects = {}
+        self._states = {}
+
+    def set_instance(self, proxy, instance):
+        self._wrapped_objects[id(proxy)] = instance
+
+    def get_instance(self, proxy):
+        return self._wrapped_objects[id(proxy)]
+
+    def del_instance(self, proxy):
+        del self._wrapped_objects[id(proxy)]
+        self._states.pop(id(proxy), None)
+
+    def get_state(self, proxy):
+        if id(proxy) not in self._states:
+            self._states[id(proxy)] = {}
+
+        return self._states[id(proxy)]
+
+
+def _resolve_without_get(cls, name):
+    for base in cls.__mro__:
+        if name in base.__dict__:
+            return base.__dict__[name]
+
+    raise AttributeError(name)
+
+
+def _mro_common(left, right):
+    left_mro = list(left.__mro__)
+    left_mro.reverse()
+
+    right_mro = list(right.__mro__)
+    right_mro.reverse()
+
+    result = [
+        left_base
+        for left_base, right_base in zip(left_mro, right_mro)
+        if left_base == right_base
+    ]
+    result.reverse()
+
+    return result
+
+
+def wrap_with(wrapped_class, proxy_class):
+    instances = _instance_wrapper()
+
+    common = _mro_common(wrapped_class, proxy_class)
+
+    base_methods = set()
+    for base in reversed(proxy_class.__mro__):
+        if base in common:
+            continue
+
+        for name in base.__dict__.keys():
+            base_methods.add(name)
+
+    resolution = {}
+    for base in reversed(wrapped_class.__mro__):
+        for name in base.__dict__.keys():
+            if name in base_methods:
+                continue
+
+            if name in {
+                "__new__",
+                "__init__",
+                "__getattr__",
+                "__setattr__",
+                "__delattr__",
+                "__getattribute__",
+            }:
+                continue
+
+            resolution[name] = base
+
+    members = {}
+    members.update(
+        {
+            name: _proxied_value(base, name, instances)
+            for name, base in resolution.items()
+            if name not in {"__getattr__", "__getattribute__"}
+        }
+    )
+    members.update(proxy_class.__dict__)
+
+    proxy_init = _resolve_without_get(proxy_class, "__init__")
+
+    def _init_wrapper(self, inner, *args, **kwargs):
+        if not isinstance(inner, wrapped_class):
+            raise TypeError(
+                "type {!r} cannot wrap object {!r} with type {!r}".format(
+                    type(self), inner, type(inner)
+                )
+            )
+        instances.set_instance(self, inner)
+        proxy_init(self, *args, **kwargs)
+
+    _init_wrapper.__name__ = "__init__"
+    members["__init__"] = _init_wrapper
+
+    @property
+    def __instance__(self):
+        return instances.get_instance(self)
+
+    members["__instance__"] = __instance__
+
+    return type(
+        "{}[{}]".format(proxy_class.__name__, wrapped_class.__name__),
+        (proxy_class,),
+        members,
+    )
+
+
+def proxy_of(wrapped_class):
+    def _decorator(proxy_class):
+        return wrap_with(wrapped_class, proxy_class)
+
+    return _decorator
+
+
+def instance(obj):
+    return obj.__instance__
---
+++ test_class_proxy.py
@@ -0,0 +1,103 @@
+import unittest
+
+from class_proxy import wrap_with
+
+
+class TestClassProxy(unittest.TestCase):
+    def setUp(self):
+        self.proxy_class = wrap_with(Base, BaseProxy)
+        self.base = Base()
+        self.instance = self.proxy_class(self.base, 1)
+
+    def test_get_proxy_class_attributes(self):
+        self.assertEqual(1, self.proxy_class.a)
+        self.assertEqual(3, self.proxy_class.b)
+        self.assertEqual(4, self.proxy_class.c)
+
+    def test_set_proxy_class_attribute(self):
+        self.proxy_class.a = 5
+
+        self.assertEqual(5, self.proxy_class.a)
+
+    def test_set_new_proxy_class_attribute(self):
+        self.proxy_class.x = 100
+
+        self.assertEqual(100, self.proxy_class.x)
+        self.assertEqual(100, self.instance.x)
+
+    def test_delete_proxy_class_attribute(self):
+        del self.proxy_class.a
+
+        self.assertRaises(AttributeError, lambda: self.proxy_class.a)
+
+    def test_get_proxy_instance_attribute(self):
+        self.assertEqual(1, self.instance.a)
+        self.assertEqual(3, self.instance.b)
+        self.assertEqual(4, self.instance.c)
+
+    def test_set_proxy_instance_attribute(self):
+        self.instance.a = 5
+
+        self.assertEqual(5, self.instance.a)
+
+    def test_set_new_proxy_instance_attribute(self):
+        self.instance.x = 100
+
+        self.assertEqual(100, self.instance.x)
+
+    def test_delete_proxy_instance_attribute(self):
+        del self.instance.a
+
+        self.assertRaises(AttributeError, lambda: self.instance.a)
+
+    def test_delete_new_proxy_instance_attribute(self):
+        self.instance.x = 100
+        del self.instance.x
+
+        self.assertRaises(AttributeError, lambda: self.instance.x)
+
+    def test_proxy_class_for_methods(self):
+        self.assertIs(self.base, self.instance.get_base())
+        self.assertIs(self.instance, self.instance.get_proxy())
+        self.assertIs(self.instance, self.instance.get_self())
+
+    def test_skip_default_object_attributes(self):
+        self.assertEqual("str-proxy", str(self.instance))
+        self.assertEqual("repr-base", repr(self.instance))
+
+    def test_error_when_trying_to_wrap_wrong_object_type(self):
+        self.assertRaises(TypeError, self.proxy_class, 0, 1)
+
+
+class Base(object):
+    a = 1
+    b = 2
+
+    def get_self(self):
+        return self
+
+    def get_base(self):
+        return self
+
+    def __str__(self):
+        return "str-base"
+
+    def __repr__(self):
+        return "repr-base"
+
+
+class BaseProxy(object):
+    b = 3
+    c = 4
+
+    def __init__(self, d):
+        self.d = d
+
+    def get_self(self):
+        return self
+
+    def get_proxy(self):
+        return self
+
+    def __str__(self):
+        return "str-proxy"
